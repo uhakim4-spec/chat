985c6d2b49a4ff822cf0889972ff6aba1fe60311
abdul hakim <uhakim4@gmail.com>
2025-09-04 10:09:15 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,19 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider, signInWithPopup, User as FirebaseUser } from "firebase/auth";
 
 const firebaseConfig = {
-  "projectId": "chatterbox-ervha",
-  "appId": "1:1012439073857:web:009b2b41ce29a7f772f2c0",
-  "storageBucket": "chatterbox-ervha.firebasestorage.app",
-  "apiKey": "AIzaSyD2ryw4xAnhnoydxMm-x7VkPgTdaqSpnqI",
-  "authDomain": "chatterbox-ervha.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1012439073857"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 
