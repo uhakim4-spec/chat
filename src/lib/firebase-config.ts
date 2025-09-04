46bc89a5e22c1ec575e01753fdc057d3508470cc
abdul hakim <uhakim4@gmail.com>
2025-09-04 18:16:04 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

---
+++ src/lib/firebase-config.ts
@@ -0,0 +1,9 @@
+// src/lib/firebase-config.ts
+export const firebaseConfig = {
+  apiKey: "AIzaSyD2ryw4xAnhnoydxMm-x7VkPgTdaqSpnqI",
+  authDomain: "chatterbox-ervha.firebaseapp.com",
+  projectId: "chatterbox-ervha",
+  storageBucket: "chatterbox-ervha.firebasestorage.app",
+  messagingSenderId: "1012439073857",
+  appId: "1:1012439073857:web:009b2b41ce29a7f772f2c0"
+};
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,28 +1,12 @@
-
 import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider, signInWithPopup, User as FirebaseUser } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { firebaseConfig } from "./firebase-config";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD2ryw4xAnhnoydxMm-x7VkPgTdaqSpnqI",
-  authDomain: "chatterbox-ervha.firebaseapp.com",
-  projectId: "chatterbox-ervha",
-  storageBucket: "chatterbox-ervha.firebasestorage.app",
-  messagingSenderId: "1012439073857",
-  appId: "1:1012439073857:web:009b2b41ce29a7f772f2c0"
-};
-
-// Function to initialize and get the Firebase app instance
-function getFirebaseApp(): FirebaseApp {
-  if (!getApps().length) {
-    return initializeApp(firebaseConfig);
-  }
-  return getApp();
-}
-
-const app = getFirebaseApp();
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const auth = getAuth(app);
 
 const provider = new GoogleAuthProvider();
 
@@ -45,3 +29,5 @@ export const signInWithGoogle = async () => {
     return null;
   }
 };
+
+export { db, auth };
