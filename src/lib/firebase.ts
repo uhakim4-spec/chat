214cc3b09b3549ecd28e3d0cd99eb4819411669b
abdul hakim <uhakim4@gmail.com>
2025-09-04 10:42:24 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, User as FirebaseUser } fr
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyD2ryw4xAnhnoydxMm-x7VkPgTdaqSpnqI",
+  authDomain: "chatterbox-ervha.firebaseapp.com",
+  projectId: "chatterbox-ervha",
+  storageBucket: "chatterbox-ervha.firebasestorage.app",
+  messagingSenderId: "1012439073857",
+  appId: "1:1012439073857:web:009b2b41ce29a7f772f2c0"
 };
 
 // Initialize Firebase
